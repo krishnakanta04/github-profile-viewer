5f2aa11f5c9445f0c2dbb387f1b2f340cb46604a
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2024-02-05 20:20:04 +0530

    Added sample git profile in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import Link from "next/link";
-
+import NextLink from "next/link";
+import { Link } from "@nextui-org/link";
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/button";
 
@@ -12,7 +12,7 @@ export default function Home() {
   const [username, setUsername] = useState<string>("");
 
   return (
-    <div className="flex flex-col gap-5 items-center justify-center mt-[100px]">
+    <div className="flex flex-col gap-7 items-center justify-center mt-[70px]">
       <Github className="h-[100px] w-[100px]" />
       <p className="text-3xl font-bold text-center">
         Explore GitHub profiles and repositories
@@ -29,13 +29,32 @@ export default function Home() {
 
       {/* if username input is empty show nothing otherwise a button to continue */}
       {!!username ? (
-        <Button color="primary" as={Link} href={`/profile/${username}`}>
+        <Button color="primary" as={NextLink} href={`/profile/${username}`}>
           Continue{" "}
           <span>
             <MoveRight />
           </span>
         </Button>
       ) : null}
+
+      {/* some sample data to try */}
+      <div className="flex flex-col items-center gap-5">
+        <p>Some sample GitHub id to explore</p>
+        <div className="flex flex-wrap items-center justify-center gap-5">
+          <Link showAnchorIcon color="primary" href="/profile/visionmedia">
+            visionmedia
+          </Link>
+          <Link showAnchorIcon color="primary" href="/profile/nwaliaez">
+            nwaliaez
+          </Link>
+          <Link showAnchorIcon color="primary" href="/profile/springmeyer">
+            springmeyer
+          </Link>
+          <Link showAnchorIcon color="primary" href="/profile/fabpot">
+            fabpot
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
