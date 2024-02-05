bfd3851ad74835396bcf335d1a68a6a038c88482
Krishnakanta Naik <krishnakantanaik04@gmail.com>
2024-02-05 17:23:06 +0530

    Added and implemented next-themes for darkmode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <ThemeSwitcher />
     </div>
   );
 }
--- src/providers/UIProvider.tsx
+++ src/providers/UIProvider.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function UIProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  );
 }
